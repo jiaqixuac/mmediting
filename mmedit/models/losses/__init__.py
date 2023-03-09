0f31e4fbec24c23c1f4bd3291133b0e035c393b1
jiaqixuac <jiaqixuac@gmail.com>
2023-03-09 17:45:20 +0800

    remove losses

--- mmedit/models/losses/__init__.py
+++ mmedit/models/losses/__init__.py
@@ -1,8 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .composition_loss import (CharbonnierCompLoss, L1CompositionLoss,
                                MSECompositionLoss)
-from .feature_loss import LightCNNFeatureLoss
-from .gan_loss import DiscShiftLoss, GANLoss, GaussianBlur, GradientPenaltyLoss
 from .gradient_loss import GradientLoss
 from .perceptual_loss import (PerceptualLoss, PerceptualVGG,
                               TransferalPerceptualLoss)
@@ -11,8 +9,8 @@ from .utils import mask_reduce_loss, reduce_loss
 
 __all__ = [
     'L1Loss', 'MSELoss', 'CharbonnierLoss', 'L1CompositionLoss',
-    'MSECompositionLoss', 'CharbonnierCompLoss', 'GANLoss', 'GaussianBlur',
-    'GradientPenaltyLoss', 'PerceptualLoss', 'PerceptualVGG', 'reduce_loss',
-    'mask_reduce_loss', 'DiscShiftLoss', 'MaskedTVLoss', 'GradientLoss',
-    'TransferalPerceptualLoss', 'LightCNNFeatureLoss'
+    'MSECompositionLoss', 'CharbonnierCompLoss',
+    'PerceptualLoss', 'PerceptualVGG', 'reduce_loss',
+    'mask_reduce_loss', 'MaskedTVLoss', 'GradientLoss',
+    'TransferalPerceptualLoss',
 ]
